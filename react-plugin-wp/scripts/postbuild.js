e82a3de6533b91d868e4065ec155a960db92564d
jbehuet <jbehuet@gmail.com>
2019-08-19 10:12:33 +0200

    update post build script

--- react-plugin-wp/scripts/postbuild.js
+++ react-plugin-wp/scripts/postbuild.js
@@ -9,12 +9,16 @@ const cssFolder = path.join(buildFolder, '/static/css');
 if (!fs.existsSync(distFolder)) fs.mkdirSync(distFolder);
 
 let files = fs.readdirSync(jsFolder);
-files.forEach(file => fs.copyFileSync(path.join(jsFolder, file), path.join(distFolder, `/${file}`)));
-//TODO: RENAME .js to plugin-name.js
+files.forEach(file => {
+    const ext = file.slice(file.lastIndexOf('.'));
+    fs.copyFileSync(path.join(jsFolder, file), path.join(distFolder, `/react-plugin-wp${ext === '.map' ? '.js.map' : '.js'}`))
+});
 
 files = fs.readdirSync(cssFolder);
-files.forEach(file => fs.copyFileSync(path.join(cssFolder, file), path.join(distFolder, `/${file}`)));
-//TODO: RENAME .css to plugin-name.css
+files.forEach(file => {
+    const ext = file.slice(file.lastIndexOf('.'));
+    fs.copyFileSync(path.join(cssFolder, file), path.join(distFolder, `/react-plugin-wp${ext === '.map' ? '.css.map' : '.css'}`))
+});
 
 fs.copyFileSync(path.join(__dirname, '../react-plugin-wp.php'), distFolder + "/react-plugin-wp.php");
 
